7d290ec077d58caab892a3c6933ffc2a6f75b9e2
Mubelotix <mubelotix@gmail.com>
2022-06-30 10:07:14 +0200

    Add stiff

--- src/kamilata.rs
+++ src/kamilata.rs
@@ -1,7 +1,6 @@
 use std::{iter, pin::Pin};
-use futures::Future;
-use libp2p::{swarm::{IntoConnectionHandler, ConnectionHandler}, core::{ConnectedPoint, upgrade::{EitherUpgrade, DeniedUpgrade}, UpgradeInfo}, InboundUpgrade, kad::protocol::{KadInStreamSink, KadOutStreamSink}, OutboundUpgrade};
-use tokio::io::{AsyncRead, AsyncWrite};
+use futures::{Future, prelude::*};
+use libp2p::{swarm::{IntoConnectionHandler, ConnectionHandler, SubstreamProtocol}, core::{ConnectedPoint, upgrade::{EitherUpgrade, DeniedUpgrade, self}, UpgradeInfo}, InboundUpgrade, kad::protocol::{KadInStreamSink, KadOutStreamSink}, OutboundUpgrade};
 
 #[derive(Debug, Clone, Default)]
 pub struct KamilataProtocolConfig {}
@@ -65,5 +64,87 @@ where
     }
 }
 
+#[derive(Debug)]
+enum KamilataHandlerIn {
 
-// Could implement UpgradeInfo and OutboundUpgrade on KamilataHandshakeOutput but no idea why
+}
+
+#[derive(Debug)]
+enum KamilataHandlerEvent {
+
+}
+
+struct KamilataHandler {
+
+}
+
+impl KamilataHandler {
+    pub fn new() -> Self {
+        KamilataHandler {
+
+        }
+    }
+}
+
+impl ConnectionHandler for KamilataHandler {
+    type InEvent = KamilataHandlerIn;
+    type OutEvent = KamilataHandlerEvent;
+    type Error = std::io::Error;
+    type InboundProtocol = EitherUpgrade<KamilataProtocolConfig, DeniedUpgrade>;
+    type OutboundProtocol = KamilataProtocolConfig;
+    type InboundOpenInfo = (); // TODO we might need things here
+    type OutboundOpenInfo = ();
+
+    fn listen_protocol(&self) -> SubstreamProtocol<Self::InboundProtocol, Self::InboundOpenInfo> {
+        SubstreamProtocol::new(KamilataProtocolConfig::new(), ()).map_upgrade(upgrade::EitherUpgrade::A)
+        // todo What is that () ?
+    }
+
+    fn inject_fully_negotiated_inbound(
+        &mut self,
+        protocol: <Self::InboundProtocol as libp2p::swarm::handler::InboundUpgradeSend>::Output,
+        info: Self::InboundOpenInfo,
+    ) {
+        // We should probably store an outbound_stream 
+        todo!()
+    }
+
+    fn inject_fully_negotiated_outbound(
+        &mut self,
+        protocol: <Self::OutboundProtocol as libp2p::swarm::handler::OutboundUpgradeSend>::Output,
+        info: Self::OutboundOpenInfo,
+    ) {
+        todo!()
+    }
+
+    fn inject_event(&mut self, event: Self::InEvent) {
+        todo!()
+    }
+
+    fn inject_dial_upgrade_error(
+        &mut self,
+        info: Self::OutboundOpenInfo,
+        error: libp2p::swarm::ConnectionHandlerUpgrErr<<Self::OutboundProtocol as libp2p::swarm::handler::OutboundUpgradeSend>::Error>,
+    ) {
+        todo!()
+    }
+
+    fn connection_keep_alive(&self) -> libp2p::swarm::KeepAlive {
+        todo!()
+    }
+
+    fn poll(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<
+        libp2p::swarm::ConnectionHandlerEvent<
+            Self::OutboundProtocol,
+            Self::OutboundOpenInfo,
+            Self::OutEvent,
+            Self::Error,
+        >,
+    > {
+        // This is where we receive shit
+        todo!()
+    }
+}
