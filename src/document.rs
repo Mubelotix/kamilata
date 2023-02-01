f0e092c14abb1a11dc5055c8d5b0c663089e4351
Mubelotix <mubelotix@gmail.com>
2023-02-01 13:15:21 +0100

    Add documentation

--- src/document.rs
+++ src/document.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 
 pub trait SearchResult: Send + Sync + 'static {
+    /// CID is required in order to detect duplicates.
     type Cid: Eq + Ord + Clone + Send + Sync + 'static;
 
     fn cid(&self) -> &Self::Cid;
@@ -12,5 +13,6 @@ pub trait Document: Send + Sync + 'static {
     type SearchResult: SearchResult;
 
     fn cid(&self) -> &<Self::SearchResult as SearchResult>::Cid;
+    // TODO: this should be async
     fn apply_to_filter(&self, filter: &mut Filter<125000>);
 }
