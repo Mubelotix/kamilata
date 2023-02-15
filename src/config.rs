fd0bac7cb53ec3289916659303cb9805b8af02f1
Mubelotix <mubelotix@gmail.com>
2023-02-15 18:57:06 +0100

    Update config

--- src/config.rs
+++ src/config.rs
@@ -51,19 +51,36 @@ impl MinTargetMax {
     }
 }
 
+/// Strategies for inserting new routing peeers.
+/// 
+/// *Inspired by [libp2p::kad::KademliaBucketInserts].*
+#[derive(Debug, Clone)]
+pub enum RoutingInserts {
+    /// Inserts all peers upon connection, as long as the limit is not reached.
+    OnConnected,
+    /// New peers and addresses are only added to the routing table via explicit calls to [Kamilata::add_address].
+    Manual
+}
+
 #[derive(Debug, Clone)]
 pub struct KamilataConfig {
-    /// Min, preferred and max values in milliseconds
+    /// Min, target and max values in milliseconds
     pub filter_update_delay: MinTargetMax,
-    /// Min, target and max number of connected peers to maintain
-    pub peer_count: MinTargetMax,
+    /// Maximum number of filters to manage per peer (default: 8)
+    pub max_filter_levels: usize,
+    /// Maximum number of peers to manage filters for (default: 20)
+    pub max_routing_peers: usize,
+    /// Strategy for inserting new routing peers (default: [OnConnected](RoutingInserts::OnConnected)))
+    pub routing_inserts: RoutingInserts
 }
 
 impl Default for KamilataConfig {
     fn default() -> Self {
         Self {
             filter_update_delay: MinTargetMax { min: 15_000, target: 20_000, max: 60_000*3 },
-            peer_count: MinTargetMax { min: 5, target: 6, max: 12 }
+            max_filter_levels: 8,
+            max_routing_peers: 20,
+            routing_inserts: RoutingInserts::OnConnected
         }
     }
 }
