b6ae8a4af5c4042d6690939f65b17ed5f21a3a12
Mubelotix <mubelotix@gmail.com>
2023-02-22 15:37:48 +0100

    Tcp + Mplex

--- tests/common/client.rs
+++ tests/common/client.rs
@@ -39,11 +39,11 @@ pub fn tcp_transport(
     let noise_keys = libp2p::noise::Keypair::<libp2p::noise::X25519Spec>::new()
         .into_authentic(&keypair)
         .expect("Signing libp2p-noise static DH keypair failed.");
-    
+
     Ok(transport
         .upgrade(libp2p::core::upgrade::Version::V1)
         .authenticate(libp2p::noise::NoiseConfig::xx(noise_keys).into_authenticated())
-        .multiplex(libp2p::yamux::YamuxConfig::default())
+        .multiplex(libp2p::mplex::MplexConfig::default())
         .timeout(std::time::Duration::from_secs(20))
         .boxed())
 }
