6f8a548b7963002775733c5b5c7c2554f122357c
Mubelotix <mubelotix@gmail.com>
2023-02-22 14:42:27 +0100

    Switch to mplex only

--- tests/common/client.rs
+++ tests/common/client.rs
@@ -26,10 +26,7 @@ pub async fn memory_transport(
     Ok(transport
         .upgrade(libp2p::core::upgrade::Version::V1)
         .authenticate(libp2p::noise::NoiseConfig::xx(noise_keys).into_authenticated())
-        .multiplex(libp2p::core::upgrade::SelectUpgrade::new(
-            libp2p::yamux::YamuxConfig::default(),
-            libp2p::mplex::MplexConfig::default(),
-        ))
+        .multiplex(libp2p::mplex::MplexConfig::default())
         .timeout(std::time::Duration::from_secs(20))
         .boxed())
 }
