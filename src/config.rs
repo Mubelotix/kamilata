efe64c0b6e4c67c3ea3ade33ec0d5020482b2ff2
Mubelotix <mubelotix@gmail.com>
2023-03-17 19:40:54 +0100

    Remove min meaning

--- src/config.rs
+++ src/config.rs
@@ -94,16 +94,16 @@ pub struct KamilataConfig {
     pub get_filters_interval: MinTargetMax,
     /// Maximum number of filters to manage per peer (default: 8)
     pub filter_count: usize,
-    /// Number of peers we receive filters from (default: 3,8,20)
+    /// Number of peers we receive filters from (default: 0,8,20)
     /// 
-    /// New peers will automatically be dialed until the `min` value is reached.
+    /// The `min` value is not used.
     /// Other peers will be requested to send us their filters until the `target` value is reached.
     /// Peers requesting us to receive their filters will be accepted until the `max` value is reached.
     /// We will stop receiving filters from peers if the `max` value is overreached.
     pub in_routing_peers: MinTargetMax,
-    /// Number of peers we send filters to (default: 4,16,50)
+    /// Number of peers we send filters to (default: 0,16,50)
     /// 
-    /// New peers will automatically be dialed until the `min` value is reached.
+    /// The `min` value is not used.
     /// Other peers will be requested to receive our filters until the `target` value is reached.
     /// Peers requesting us our filters will be accepted until the `max` value is reached.
     /// We will stop sending filters to peers if the `max` value is overreached.
@@ -115,8 +115,8 @@ impl Default for KamilataConfig {
         Self {
             get_filters_interval: MinTargetMax { min: 15_000, target: 20_000, max: 60_000*3 },
             filter_count: 8,
-            in_routing_peers: MinTargetMax { min: 3, target: 6, max: 20 },
-            out_routing_peers: MinTargetMax { min: 3, target: 12, max: 50 },
+            in_routing_peers: MinTargetMax { min: 0, target: 6, max: 20 },
+            out_routing_peers: MinTargetMax { min: 0, target: 12, max: 50 },
         }
     }
 }
